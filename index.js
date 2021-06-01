8a1ac96034b86bc6c031a7b8d4e2058d05793852
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-06-01 19:59:37 +0930

    adding the last method of calculatePayroll

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* Your Code Here */
-
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -9,14 +8,73 @@
  for you to use if you need it!
  */
 
+let createEmployeeRecord = function (recordArray) {
+  return {
+    firstName: recordArray[0],
+    familyName: recordArray[1],
+    title: recordArray[2],
+    payPerHour: recordArray[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+};
+
+let createEmployeeRecords = function (recordsArray) {
+  return recordsArray.map((record) => createEmployeeRecord(record));
+};
+
+let createTimeInEvent = function (timeStamp) {
+  let [date, hour] = timeStamp.split(" ");
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+};
+
+let createTimeOutEvent = function (timeStamp) {
+  let [date, hour] = timeStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+};
+
+let hoursWorkedOnDate = function (date) {
+  let inHour = this.timeInEvents.find((e) => e.date === date).hour;
+  let outHour = this.timeOutEvents.find((e) => e.date === date).hour;
+
+  return (outHour - inHour) / 100;
+};
+
+let wagesEarnedOnDate = function (date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+};
+
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  let payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+  return payable;
+};
+
+let findEmployeeByFirstName = function (srcArray, firstName) {
+    return srcArray.find((employee) => employee.firstName === firstName);
+};
+
+let calculatePayroll = function (recordsArray) {
+    
 }
